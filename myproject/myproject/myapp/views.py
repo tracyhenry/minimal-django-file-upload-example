7eb8d3864c9b415fe3de4eb24f9174c6fdbf9be5
Wenbo Tao <thierryhenrytracy@163.com>
2016-03-05 13:32:06 +0800

    Try.

--- myproject/myproject/myapp/views.py
+++ myproject/myproject/myapp/views.py
@@ -7,7 +7,20 @@ from django.core.urlresolvers import reverse
 from myproject.myapp.models import Document
 from myproject.myapp.forms import DocumentForm
 
+def get_table_context(f)
+
+    print f.read()
+    return {"schema_list" : [{"col" : 1, "value" : "Country"},
+                             {"col" : 2, "value" : "Population"}],
+                             
+            "row_list" : [{"row" : 1, "cell" : [{"col" : 1, "value" : "China"},
+                                                {"col" : 2, "value" : "20"}]},
+                          {"row" : 2, "cell" : [{"col" : 1, "value" : "US"},
+                                                {"col" : 2, "value" : "30"}]}]}
+    
 def list(request):
+
+    context = {}
     # Handle file upload
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
@@ -15,17 +28,23 @@ def list(request):
             newdoc = Document(docfile = request.FILES['docfile'])
             newdoc.save()
 
+            context.update(get_table_context(request.FILES['docfile']))
+            context["has_table"] = True
             # Redirect to the document list after POST
-            return HttpResponseRedirect(reverse('myproject.myapp.views.list'))
+            # return HttpResponseRedirect(reverse('myproject.myapp.views.list'))
     else:
         form = DocumentForm() # A empty, unbound form
+        context["has_table"] = False
 
     # Load documents for the list page
     documents = Document.objects.all()
 
     # Render list page with the documents and the form
+    context["documents"] = documents
+    context["form"] = form
+    
     return render_to_response(
         'myapp/list.html',
-        {'documents': documents, 'form': form},
+        context,
         context_instance=RequestContext(request)
     )
